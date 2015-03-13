235dc4c95d474309ef0d9be2ccf66c9d00e72fa3
Rafael Auler <rafaelauler@gmail.com>
2015-03-12 17:08:16 -0700

    Implement GEP removal pass

--- tools/static-bt/OiCombinePass.cpp
+++ tools/static-bt/OiCombinePass.cpp
@@ -9,12 +9,20 @@
 
 #include "OiCombinePass.h"
 #include "llvm/IR/PatternMatch.h"
+#include "llvm/IR/IRBuilder.h"
 #include "llvm/Support/raw_ostream.h"
+
+#define COMBINE2
 
 using namespace llvm;
 using namespace PatternMatch;
 
-static void OiCombine(Instruction *v) {
+static unsigned numMatches1 = 0;
+#ifdef COMBINE2
+static unsigned numMatches2 = 0;
+#endif
+
+static void OiCombine(Instruction *v, IRBuilder<> &Builder) {
   Value *X, *Y;  
   if (match(v, m_Add(m_Shl(m_LShr(m_Value(X),
                                   m_ConstantInt<16>()),
@@ -23,21 +31,61 @@ static void OiCombine(Instruction *v) {
                            m_ConstantInt<0xFFFF>())))) {
     //    if (X == Y) {
       v->replaceAllUsesWith(Y);
+      ++numMatches1;
       //    }
   }
+
+#ifdef COMBINE2
+  if (auto *GEP = dyn_cast<GetElementPtrInst>(v)) {
+    if (GEP->getNumIndices() < 2)
+      return;
+    auto *AddInst = dyn_cast<Instruction>(GEP->getOperand(2));
+    if (!AddInst)
+      return;
+
+    if (auto *Ptr = dyn_cast<Constant>(GEP->getPointerOperand())) {
+      ConstantInt *CI;
+      if (match(AddInst, m_Add(m_Value(X), m_ConstantInt(CI)))) {
+        Builder.SetInsertPoint(AddInst);
+//      AddInst->dump();
+//      GEP->dump();
+        Value *V1 = Builder.CreatePtrToInt(Ptr, Type::getInt32Ty(getGlobalContext()));
+        AddInst->setOperand(0, V1);
+        Builder.SetInsertPoint(v);
+        Value *V2 = Builder.CreateAdd(AddInst, X);
+        Value *AddCast = Builder.CreateIntToPtr(
+            V2, Type::getInt8PtrTy(getGlobalContext()));
+//     V2->dump();
+       GEP->replaceAllUsesWith(AddCast);
+        ++numMatches2;
+      }
+    }
+  }
+#endif
 }
 
 bool OiCombinePass::runOnFunction(Function &F) {
+  IRBuilder<> Builder(getGlobalContext());
   errs() << "Hello: ";
   errs().write_escaped(F.getName()) << '\n';
 
   for (Function::iterator FI = F.begin(), FE = F.end(); FI != FE; ++FI) {
     for (BasicBlock::iterator BI = FI->begin(), BE = FI->end(); BI != BE; ++BI){
-      OiCombine(&*BI);
+      OiCombine(&*BI, Builder);
     }
   }
-  return false;
+
+  errs() << "Number of load large immediates recombined: " << numMatches1 << "\n";
+#ifdef COMBINE2
+  errs() << "Number of GEP+ShadowMemory recombined: " << numMatches2 << "\n";
+  return numMatches1 + numMatches2 > 0;
+#else
+  return numMatches1 > 0;
+#endif
+
 }
 
 char OiCombinePass::ID = 0;
 static RegisterPass<OiCombinePass> X("oicombinepass", "OpenISA-specific Combine Pass", false, false);
+
+#undef COMBINE2
